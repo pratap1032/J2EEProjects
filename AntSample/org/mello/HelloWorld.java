bd452821d8f1d191fc810c076485af69341cfb52
Pratap Raj <pratap1032@gmail.com>
2012-02-19 02:27:51 +0530

    First commit on AntSample project
    Signed-off-by: Pratap Raj

---
+++ AntSample/org/mello/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HElloWorld");
+
+	}
+
+}
---
+++ AntSample/org/mello/HelloWorld2.java
@@ -0,0 +1,12 @@
+package com.hello;
+
+public class HelloWorld2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HElloWorld");
+	}
+
+}
